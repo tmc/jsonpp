615634ed3896d6e1789281f254528fc25d64fc5a
Travis Cline <travis.cline@gmail.com>
2013-09-05 00:39:56 -0500

    add interactivity check

--- cmd/jsonpp/main.go
+++ cmd/jsonpp/main.go
@@ -8,7 +8,20 @@ import (
 	"os"
 )
 
+// Return true if os.Stdin appears to be interactive
+func isInteractive() bool {
+	fileInfo, err := os.Stdin.Stat()
+	if err != nil {
+		return false
+	}
+	return fileInfo.Mode()&(os.ModeCharDevice|os.ModeCharDevice) != 0
+}
+
 func main() {
+	if isInteractive() {
+		fmt.Fprintln(os.Stderr, os.Args[0], "expects stdin")
+		os.Exit(1)
+	}
 	in, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "error reading stdin:", err)
